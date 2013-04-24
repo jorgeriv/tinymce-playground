d39ad59fbddc4166ad9b8d6b3ce5640fdb5d03f1
Jorge <jorgeriv@gmail.com>
2013-04-24 17:17:11 -0400

    add some validations to the RTE plugin

--- public/js/app/RTE_Module/app.js
+++ public/js/app/RTE_Module/app.js
@@ -1,13 +1,14 @@
-define(['tinymce', 'jquery', 'jquery.tinymce'], function(tinymce, $) {
-	var gSettings = {};
-	gSettings.defaults = {
+define(['tinymce', 'jquery', 'underscore', 'jquery.tinymce'], function(tinymce, $, _) {
+	var gSettings = {}, // global settings
+		editors = []; // active editors ID's array
+
+	gSettings.defaults = { // default settings
 		mode: "none",
 		theme: "simple"
 	};
 
-	gSettings.popup = {
+	gSettings.popup = { // custom settings
 		plugins: '-example',
-		// - tells TinyMCE to skip the loading of the plugin
 		mode: "textareas",
 		theme: "advanced",
 		theme_advanced_buttons1: "mylistbox,mysplitbutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink",
@@ -37,28 +38,49 @@ define(['tinymce', 'jquery', 'jquery.tinymce'], function(tinymce, $) {
 				defaul: settings = gSettings.defaults;
 				break;
 			}
-			//TODO: verifty if the target is already an editor, if so update settings and repaint;
-			//TODO: verify if target is an id
-			tinymce.execCommand('mceAddControl', false, target);
+
+			if(typeof target === 'string'){
+
+				if(_.contains(editors, target)){
+
+					tinymce.execCommand('mceRepaint'); //if editor already exists, repaint
+
+				} else {
+
+					tinymce.execCommand('mceAddControl', false, target); // if editor doesn't exist, add
+					editors.push(target);
+
+				}
+			}
 		},
 
 		removeEditor: function(target) {
+
 			//TODO: verifty if the target is already an editor, if not, skip
 			if (typeof target === 'string') {
+				editors = _.reject(editors, function(item){
+					return tinymce.activeEditor.id === item;
+				});
 				tinyMCE.execCommand('mceFocus', false, target);
 				tinyMCE.execCommand('mceRemoveControl', false, target);
 			}
 		},
 
 		removeActive: function() {
+
 			if (tinymce.activeEditor) {
+				editors = _.reject(editors, function(item){
+					return tinymce.activeEditor.id === item;
+				});
 				tinymce.activeEditor.remove();
 			}
 		},
 
 		addSettings: function(id, settings) {
+
 			id = id || null;
 			settings = settings || null;
+
 			if (typeof settings === 'object' && typeof id === 'string') {
 				gSettings[id] = settings;
 			}
